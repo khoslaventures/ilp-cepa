373426ea779f6513395ed88f79db7c88f930ce1c
Akash Khosla <me@akashkhosla.com>
2018-12-06 16:12:34 -0800

    Add tf scripts and supported server

--- run-server.js
+++ run-server.js
@@ -1,9 +1,49 @@
+const {
+  CepaServer
+} = require('./src/cepa-server')
+
 // Look for file in directory. If not there, run with null params.
 // Null params means this is an end server.
 // Read address and secret from json file
-input_secret = ""
 
-function
-let cepa = new CepaServer('End', null, null)
-await cepa.ServerSetup() // setup server secret and address
-cepa.Run()
+const fs = require('fs')
+
+const inputSecretAndAddress = './inputaddrsecret.json'
+const outputFile = './serveraddrsec.json'
+
+async function run () {
+  var nextHopSharedSecret = null
+  var nextHopAddress = null
+  if (fs.existsSync(inputSecretAndAddress)) {
+    const rawData = fs.readFileSync(inputSecretAndAddress)
+    const jsonData = JSON.parse(rawData)
+    nextHopSharedSecret = Buffer.from(jsonData.shared_secret)
+    nextHopAddress = jsonData.destination_account
+  } else {
+    console.log('CEPA-Server: First Server!')
+  }
+
+  // Would be smart if TF would send the name over.
+  var cepa = new CepaServer('CEPA', nextHopSharedSecret, nextHopAddress)
+  await cepa.ServerSetup() // setup server secret and address
+
+  // Output the server's secret and address
+  const outputSecretAndAddress = {
+    destination_account: cepa.address,
+    shared_secret: cepa.secret
+  }
+  const outputData = JSON.stringify(outputSecretAndAddress)
+
+  fs.writeFileSync(outputFile, outputData) // not best practice but easier
+
+  cepa.Run()
+  // constructor(serialized_input) {
+  //   const json = JSON.parse(serialized_input)
+  //   console.log(serialized_input)
+  //   this.sharedSecret = Buffer.from(json.shared_secret.data)
+  //   this.destinationAccount = json.destination_account
+  //   this.connection = null
+  // }
+}
+
+run().catch((err) => console.log(err))
--- terraform/server/server.tf
+++ terraform/server/server.tf
@@ -1,16 +1,58 @@
+# Suppose I have a topology like such:
+
+# Client -> CEPA2 -> CEPA1 -> CEPA0
+
+# Terraform sets up CEPA1. CEPA1 generates shared secret and address, outputs it to file.
+# Terraform SCPs the shared secret and address to local, calls it cepa1addsec.json
+
+# Terraform sets up CEPA2, by importing the cepa2addsec.json onto the server.
+# CEPA2 generates shared secret and address, outputs it to file. Terraform
+# SCPs the shared secret and address to local, calls it cepa2addsec.json.
+
+# Terraform sets up CEPA3, by importing the cepa2addsec.json onto the server,
+# CEPA3 generates shared secret and address, outputs it to file. Terraform
+# SCPs the shared secret and address to local, calls it cepa3addsec.json.
+
+# Client imports cepa*addsec.json onto their server. Client is designed to read
+# through all of these, and has a function that will handle the JSONs and a
+# constructor that takes an arbitrary number of secret/address pairs. Client handles in
+# descending order
+
+# TODO: Parameterize more things
 variable "user" {
   default = "root"
 }
 
-resource "digitalocean_droplet" "end" {
+# TODO: useful for distributed nodes
+variable "regions" {
+  type = "list"
+  default = ["nyc1, sfo1"]
+}
+
+variable "prefix" {
+  default = "cepa"
+}
+variable "resource_count" {
+  default = 3
+}
+
+resource "digitalocean_droplet" "servers" {
+  count = "${var.resource_count}" # always incrementing, starting from 1
   image = "ubuntu-18-04-x64"
-  name = "end"
+  name = "${var.prefix}${count.index}"
   region = "nyc1"
-  size = "512mb"
+  size = "1GB"
   private_networking = true
   ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
+
+#  Could have client async wait for files to be produced, and then run the stuff.
+#  Should ignore/send dummy file when ${count.index} is 0
+  provisioner "file" {
+    source      = "${count.index != 0 ? "${var.prefix}${count.index - 1}addrsec.json" : "dummy.json"}"
+    destination = "${count.index != 0 ? "/root/inputaddrsecret.json" : "/root/dummy.json"}"
+  }
 
   connection {
     user = "${var.user}"
@@ -22,19 +64,21 @@ resource "digitalocean_droplet" "end" {
   provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
-      "sudo apt-get update",
+      "sudo apt -qq update",
       "sudo apt -qq install -y nodejs npm",
       # Install and run moneyd
       "npm install -g moneyd moneyd-uplink-xrp --silent", # remove silent if bugs
       "moneyd xrp:configure --testnet",
-      "screen -S moneyd -dm moneyd xrp:start --testnet", # May need to wait?
-      "git clone https://github.com/khoslaventures/ilp-cepa.git",
+      "screen -S moneyd -dm moneyd xrp:start --testnet",
+      "git clone -b akash/tf https://github.com/khoslaventures/ilp-cepa.git",
       "cd ilp-cepa",
-      "npm "
+      "npm install --silent",
+      "[ -f /root/inputaddrsecret.json ] && mv /root/inputaddrsecret.json .", // check if file exists and then move.
+      "node run-server.js"
     ]
   }
 
   provisioner "local-exec" {
-    command ="scp -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 ${var.user}@${digitalocean_droplet.end.ipv4_address}:~/ilp-cepa/package.json ${digitalocean_droplet.end.name}.json"
+    command ="scp -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 ${var.user}@${digitalocean_droplet.servers.ipv4_address}:~/ilp-cepa/serveraddrsec.json ${digitalocean_droplet.servers.name}addrsec.json"
   }
 }
