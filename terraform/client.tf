45f7337ddb53c49cc40d8148d9f5723923ed6b04
Akash Khosla <me@akashkhosla.com>
2018-12-11 22:03:49 -0800

    Update terraform setup
    * Add Makefile for running terraform setup
    * Add python connect script for setting up servers
    * Clean out other pointless directories and consolidate

---
+++ terraform/client.tf
@@ -0,0 +1,4 @@
+# Objective: Roll a server up that will run a client
+# Client will be provided with an arglist of the .json files
+# Client will really only need the destinations of the future nodes
+# TODO: Write script that will throw destinations over to the client
--- terraform/server.tf
+++ terraform/server.tf
@@ -18,22 +18,18 @@
 # constructor that takes an arbitrary number of secret/address pairs. Client handles in
 # descending order
 
-# TODO: Parameterize more things
-variable "user" {
-  default = "root"
+variable "user" {}
+variable "resource_count" {}
+variable "prefix" {}
+variable "commands" {
+  type="list"
 }
+
 
 # TODO: useful for distributed nodes
 variable "regions" {
   type = "list"
   default = ["nyc1, sfo1"]
-}
-
-variable "prefix" {
-  default = "cepa"
-}
-variable "resource_count" {
-  default = 3
 }
 
 resource "digitalocean_droplet" "servers" {
@@ -56,22 +52,7 @@ resource "digitalocean_droplet" "servers" {
 
 
   provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      "sudo apt -qq update",
-      "sudo apt -qq install -y nodejs npm tmux",
-      # Install and run moneyd
-      "npm install -gs moneyd moneyd-uplink-xrp", # remove silent if bugs
-      "moneyd xrp:configure --testnet",
-      "screen -S moneyd -dm moneyd xrp:start --testnet",
-      "git clone -b akash/tf https://github.com/khoslaventures/ilp-cepa.git",
-      "cd ilp-cepa",
-      "npm install -s",
-      "tmux start-server",
-      "tmux new-session -d -s session",
-      "tmux new-window -t session:1",
-      "tmux send-keys -t session:1 node Space run-server.js C-m"
-    ]
+    inline = ["${var.commands}"]
   }
 
   provisioner "local-exec" {
